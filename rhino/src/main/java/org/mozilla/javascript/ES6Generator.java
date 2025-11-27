c74cd919b61f431eb0d190915a76506870461fd8
satish.srinivasan <satish.srinivasan@servicenow.com>
2025-11-27 07:50:41 +0530

    Implement Symbol.toStringTag for generators

--- rhino/src/main/java/org/mozilla/javascript/ES6Generator.java
+++ rhino/src/main/java/org/mozilla/javascript/ES6Generator.java
@@ -40,6 +40,9 @@ public final class ES6Generator extends ScriptableObject {
                 new LambdaFunction(scope, "[Symbol.iterator]", 0, ES6Generator::js_iterator);
         prototype.defineProperty(SymbolKey.ITERATOR, iterator, DONTENUM);
 
+        prototype.defineProperty(
+                SymbolKey.TO_STRING_TAG, prototype.getClassName(), DONTENUM | READONLY);
+
         if (sealed) {
             prototype.sealObject();
         }
